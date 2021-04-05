8dd7f93c8aa23b29b1b57696b7e9f6fb71920ffe
nima shahrokhkhani <nima.shahrokhkhani@gmail.com>
2021-04-05 14:35:27 +0430

    revision

--- src/market/Routes.js
+++ src/market/Routes.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import {Router, Switch, Route} from "react-router-dom";
-import Home from "./Home/Home.js";
 
 
 import history from '../history';
@@ -12,7 +11,6 @@ export default class Routes extends Component {
             <Router history={history}>
                 <ScrollToTop>
                     <Switch>
-                        <Route path="/Market" exact component={Home}/>
                     </Switch>
                 </ScrollToTop>
             </Router>
